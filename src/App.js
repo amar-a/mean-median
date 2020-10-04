66cab4f09092c4d1cbdc5cb19b7923f8d0c0185d
Amar Anand <71566509+amar-a@users.noreply.github.com>
2020-10-04 11:25:41 -0400

    Calculate mean and median

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Container from '@material-ui/core/Container';
+import Input from '@material-ui/core/Input';
+import Box from '@material-ui/core/Box';
+import { red } from '@material-ui/core/colors';
+
 function App() {
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [n3, setN3] = useState(0);
+
+  const onN1Change = event => setN1(parseInt(event.target.value))
+
+  const onN2Change = event => setN2(parseInt(event.target.value))
+
+  const onN3Change = event => setN3(parseInt(event.target.value))
+
+  const calculateMean = () => (n1+n2+n3)/3
+
+  const calculateMedian = () => [n1, n2, n3].sort()[1]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Box m={1} border={1} borderColor={red}>
+            <h1>Calculate Mean and Median</h1>
+          </Box>
+        </Container>
       </header>
+
+      <Box display={"flex"} justifyContent={"center"}>
+        <Box m={1}>
+          <Input type="number" id="number1" label="#1" value={n1} onChange={onN1Change} />
+        </Box>
+        <Box m={1}>
+          <Input type="number" id="number2" label="#2" value={n2} onChange={onN2Change} />
+        </Box>
+        <Box m={1}>
+          <Input type="number" id="number3" label="#3" value={n3} onChange={onN3Change} />
+        </Box>
+      </Box>
+
+      <Box display={"flex"} justifyContent={"center"}>
+        <h1>Mean: {calculateMean()}</h1> 
+      </Box>
+      <Box display={"flex"} justifyContent={"center"}>
+        <h1>Median: {calculateMedian()}</h1> 
+      </Box>
     </div>
   );
 }
